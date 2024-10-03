782deaf23ec3218babd5b41f688af6e723e6a9f9
ZaidRaIder <yamanijacky73@gmail.com>
2024-10-03 20:00:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pelanggan Terhormat</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    </head>
+    <body>
+        <div class="nav-top">
+            <div class="logo-container">
+                <img src="images/logo.png" alt="Logo" class="logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="pelanggan.php">Registrasi</a></li>
+                    <li><a href="properti.php">Properti</a></li>
+                    <li><a href="pembayaran.php">Pembayaran</a></li>
+                   
+                </ul>
+            </nav>
+        </div>
+
+        <header class="home" style="max-width:1580px;">
+            <div class="overlay">
+                <img src="images/HomeBg.jpg" alt="Home" class="home-image" width="1580" height="800" style="object-fit: cover;">
+            </div>
+            <div class="content">
+                <h1>Temukan Rumah Impian Anda<br>Bersama Adamaliq</h1>
+                <a href="pelanggan.php" class="findhome-btn">register</a>
+            </div>
+        </header>
+
+    
+
+        <div class="footer-container">
+            <h4>&copy; 2023 adamaliq</h3>
+        </div>
+
+    </body>
+
+</html>
